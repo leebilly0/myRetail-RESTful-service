8bb910d83ea7b32132dbafb9783e186b2254af0c
Billy Lee <lee.billy427@gmail.com>
2018-05-16 00:53:53 -0500

    added updated project name and code

---
+++ product/src/main/java/com/example/demo/Product.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+	@Id
+	private String id;
+	private String name;
+	private CurrentPrice current_price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public CurrentPrice getCurrent_price() {
+		return current_price;
+	}
+
+	public void setCurrent_price(CurrentPrice current_price) {
+		this.current_price = current_price;
+	}
+}
